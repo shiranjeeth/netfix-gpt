2f1420c87dc955807e9cab3083d4ae7e2dff070f
shiranjeeth <shiranjeeth619@gmail.com>
2024-12-10 19:51:11 +0530

    feat :created a CustomHook to get the movie trailer and making the component clean

---
+++ src/Hooks/useMovieTrailervideo.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { API_OPTIONS } from '../utils/Constants'
+import { useDispatch } from 'react-redux'
+import { addTrailerVideo } from '../utils/Slices/MovieSlice';
+
+const useMovieTrailervideo = ({movieID}) => {
+
+    const dispatch = useDispatch();
+    
+    const getMovieVideos = async ()=>{
+     const data = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos?language=en-US`,API_OPTIONS);
+     const json = await data.json()
+    //  console.log(json);
+     const Trailer = json.results.filter((videos)=>videos.type === "Trailer");  // get the trailer
+     const Teaser = json.results.filter((videos)=>videos.type === "Teaser");  // get the teaser
+    //  console.log(Teaser);
+     const officialTrailer = Trailer ? Trailer[0] : Teaser[0] ;
+    //console.log(officialTrailer);
+    dispatch(addTrailerVideo(officialTrailer));
+    }
+  
+    useEffect(()=>{
+      getMovieVideos();
+    },[])
+}
+
+export default useMovieTrailervideo
--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,29 +1,18 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/Constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addTrailerVideo } from '../utils/Slices/MovieSlice';
+import React from 'react'
+import {  useSelector } from 'react-redux'
+import useMovieTrailervideo from '../Hooks/useMovieTrailervideo';
 
 const VideoBackground = ({movieID}) => {
-  const dispatch = useDispatch();
-  const trailerVideoFromStore = useSelector((store)=>store.movies?.TrailerVideo)
-  const getMovieVideos = async ()=>{
-   const data = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos?language=en-US`,API_OPTIONS);
-   const json = await data.json()
-  //  console.log(json);
-   const Trailer = json.results.filter((videos)=>videos.type === "Trailer");  // get the trailer
-   const Teaser = json.results.filter((videos)=>videos.type === "Teaser");  // get the teaser
-  //  console.log(Teaser);
-   const officialTrailer = Trailer ? Trailer[0] : Teaser[0] ;
-  //console.log(officialTrailer);
-  dispatch(addTrailerVideo(officialTrailer));
-  }
+ 
+ /// fetches my trailer video
+  useMovieTrailervideo({movieID});
 
-  useEffect(()=>{
-    getMovieVideos();
-  },[])
+  const trailerVideoFromStore = useSelector((store)=>store.movies?.TrailerVideo)
   return (
 <>
 <iframe width="560" height="315" src={`https://www.youtube.com/embed/${trailerVideoFromStore.key}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
 </>
   )
 }
