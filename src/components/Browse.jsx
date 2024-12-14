740f8d7213f6cc86888264688fbc0588c25d534e
shiranjeeth <shiranjeeth619@gmail.com>
2024-12-15 00:34:34 +0530

    Called the custom hooks in browse page so that the cards can render in UI

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import useNowPlayingMovies from '../Hooks/useNowPlayingMovies'
+import usePopularMovies from '../Hooks/usePopularMovies'
+import useTopRated from '../Hooks/useTopRated'
+import useUpcoming from '../Hooks/useUpcoming'
 import MainContainer from './MainContainer';
 import SecondaryContainer from './SecondaryContainer';
 
+
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRated();
+  useUpcoming();
  
   return (
    <>
